1bc917a5f92bcc9e8422ff7ae69cec2d12db7e91
Parham2002 <parhamm2002@gmail.com>
2022-12-12 13:56:41 +0000

    calculator works

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,50 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
+
+    static void calculator() {
+        Scanner scanner = new Scanner(System.in);
+        int result = 0;
+
+        while (true) {
+            System.out.println("Input first value: ");
+            int valOne = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Input operator: ");
+            String operator = scanner.nextLine();
+            System.out.println("Input second value: ");
+            int valTwo = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (operator) {
+                case "+":
+                    result = valOne + valTwo;
+                    break;
+                case "-":
+                    result = valOne - valTwo;
+                    break;
+                case "*":
+                    result = valOne * valTwo;
+                    break;
+                case "/":
+                    result = valOne / valTwo;
+                    break;
+                default:
+                    System.out.println("Wrong output.");
+                    continue;
+            }
+
+            System.out.printf("The result of (%d %s %d) is %d", valOne, operator, valTwo, result);
+            System.out.println();
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main.calculator();
     }
 }
